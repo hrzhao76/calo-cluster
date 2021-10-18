57ac2df48e6f3763a213a7f960164b1ddcb978c4
Haoran Zhao <hrzhao76@gmail.com>
2021-10-17 21:52:04 -0700

    matching the AMVF_reco_ID from reco cluster

--- calo_cluster/datasets/mixins/vertex.py
+++ calo_cluster/datasets/mixins/vertex.py
@@ -22,6 +22,18 @@ class VertexDatasetMixin(PandasDataset):
         return super().__post_init__()
 
 
+def get_match_idx(truth_z0_list, reco_z0_list, reco_vtxID_list):
+    match_idx = []
+    for truth_z0 in truth_z0_list:
+        reco_idx = np.where(reco_z0_list == truth_z0)
+        if reco_idx[0].size != 0:
+            match_idx.append(reco_vtxID_list[reco_idx[0][0]]) # Here we just save the first 
+        else:
+            match_idx.append(-1)
+            
+    return match_idx
+
+
 @dataclass
 class VertexDataModuleMixin(BaseDataModule):
     instance_target: str
@@ -34,25 +46,36 @@ class VertexDataModuleMixin(BaseDataModule):
         return kwargs
 
     @staticmethod
-    def root_to_pickle(root_data_path, raw_data_dir, noise_id=-99, pf_noise_id=-1):
+    def root_to_pickle(root_data_path, raw_data_dir):
         ni = 0
         for f in sorted(root_data_path.glob('*.root')):
             root_dir = uproot.open(f)
-            root_events = root_dir.get('Events;1')
-            data_dict = {}
-            for k, v in tqdm(root_events['RecHit'].items()):
-                data_dict[k.split('.')[1]] = v.array()
+            truth_tree = root_dir['Truth_Vertex_PV_Selected']
+            reco_tree = root_dir['Reco_Vertex']
+            truth_jagged_dict = {}
+            reco_jagged_dict = {}
+            truth_prefix = 'truth_vtx_fitted_trk_'
+            reco_prefix = 'reco_vtx_fitted_trk_'
 
-            for n in tqdm(range(len(data_dict[list(data_dict.keys())[0]]))):
-                df_dict = {k: data_dict[k][n] for k in data_dict.keys()}
+            for k, v in tqdm(truth_tree.items()):
+                if not k.startswith(truth_prefix):
+                    continue
+                truth_jagged_dict[k[len(truth_prefix):]] = v.array()
+            
+            for k, v in tqdm(reco_tree.items()):
+                if not k.startswith(reco_prefix):
+                    continue
+                reco_jagged_dict[k[len(reco_prefix):]] = v.array()
+            
+            truth_jagged_dict['truth_vtxID'] = truth_jagged_dict.pop('vtxID')
+
+            coords = ['d0', 'z0', 'phi', 'theta', 'qp']
+            scale = np.array([0.05, 500, 6, 2, 4])
+            for n in tqdm(range(len(reco_tree[0].array()))):
+                df_dict = {k: truth_jagged_dict[k][n] for k in truth_jagged_dict.keys()}
                 flat_event = pd.DataFrame(df_dict)
-                pf_noise_mask = (flat_event['PFcluster0Id'] == pf_noise_id)
-                flat_event['pf_hit'] = 0
-                flat_event.loc[~pf_noise_mask, 'pf_hit'] = 1
-                flat_event.astype({'hit': int})
-                hit_mask = (flat_event['genE'] > 0.2)
-                noise_mask = ~hit_mask
-                flat_event['hit'] = hit_mask.astype(int)
-                flat_event.loc[noise_mask, 'trackId'] = noise_id
+                match_idx = get_match_idx(np.array(truth_jagged_dict['z0'][n]), np.array(reco_jagged_dict['z0'][n]), np.array(reco_jagged_dict['vtxID'][n]))
+                flat_event['AMVF_reco_ID'] = match_idx
+                flat_event[coords] /= scale
                 flat_event.to_pickle(raw_data_dir / f'event_{n+ni:05}.pkl')
             ni += n + 1
