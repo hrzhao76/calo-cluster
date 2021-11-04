36ad60bccdb060354a1af5adc5922c1399c8b762
hrzhao76 <hrzhao76@gmail.com>
2021-11-04 05:43:47 +0000

    adapt to aws

--- calo_cluster/datasets/mixins/vertex.py
+++ calo_cluster/datasets/mixins/vertex.py
@@ -5,6 +5,24 @@ import uproot
 from calo_cluster.datasets.base import BaseDataModule
 from calo_cluster.datasets.pandas_data import PandasDataModuleMixin, PandasDataset
 from tqdm.auto import tqdm
+import numpy as np
+
+def get_match_idx(truth_flat_event, reco_flat_event):
+    match_idx = []
+    
+    truth_trk_table = truth_flat_event[['d0','z0','phi','theta','qp']].to_numpy()
+    reco_trk_table = reco_flat_event[['d0','z0','phi','theta','qp']].to_numpy()
+    
+    reco_vtxID_list = reco_flat_event['vtxID'].to_numpy()
+    
+    for truth_trk in truth_trk_table:
+        reco_idx = np.flatnonzero((reco_trk_table == truth_trk).all(1))
+        if reco_idx.size != 0:
+            match_idx.append(reco_vtxID_list[reco_idx[0]]) # Here we just save the first 
+        else:
+            match_idx.append(-1)
+            
+    return match_idx
 
 
 @dataclass
@@ -13,10 +31,11 @@ class VertexDatasetMixin(PandasDataset):
 
     def __post_init__(self):
         if self.instance_target == 'reco':
-            self.semantic_label = 'semantic_label'
+            self.semantic_label = 'reco_semantic_label'
             self.instance_label = 'reco_vtxID'
         elif self.instance_target == 'truth':
-            raise NotImplementedError()
+            self.semantic_label = 'truth_semantic_label'
+            self.instance_label = 'truth_vtxID'
         else:
             raise RuntimeError()
         return super().__post_init__()
@@ -34,25 +53,46 @@ class VertexDataModuleMixin(PandasDataModuleMixin, BaseDataModule):
         return kwargs
 
     @staticmethod
-    def root_to_pickle(root_data_path, raw_data_dir, noise_id=-99, pf_noise_id=-1):
+    def root_to_pickle(root_data_path, raw_data_dir):
         ni = 0
         for f in sorted(root_data_path.glob('*.root')):
             root_dir = uproot.open(f)
-            root_events = root_dir.get('Events;1')
-            data_dict = {}
-            for k, v in tqdm(root_events['RecHit'].items()):
-                data_dict[k.split('.')[1]] = v.array()
+            truth_tree = root_dir['Truth_Vertex_PV_Selected']
+            reco_tree = root_dir['Reco_Vertex']
+            truth_jagged_dict = {}
+            reco_jagged_dict = {}
+            truth_prefix = 'truth_vtx_fitted_trk_'
+            reco_prefix = 'reco_vtx_fitted_trk_'
 
-            for n in tqdm(range(len(data_dict[list(data_dict.keys())[0]]))):
-                df_dict = {k: data_dict[k][n] for k in data_dict.keys()}
-                flat_event = pd.DataFrame(df_dict)
-                pf_noise_mask = (flat_event['PFcluster0Id'] == pf_noise_id)
-                flat_event['pf_hit'] = 0
-                flat_event.loc[~pf_noise_mask, 'pf_hit'] = 1
-                flat_event.astype({'hit': int})
-                hit_mask = (flat_event['genE'] > 0.2)
-                noise_mask = ~hit_mask
-                flat_event['hit'] = hit_mask.astype(int)
-                flat_event.loc[noise_mask, 'trackId'] = noise_id
-                flat_event.to_pickle(raw_data_dir / f'event_{n+ni:05}.pkl')
+            for k, v in tqdm(truth_tree.items()):
+                if not k.startswith(truth_prefix):
+                    continue
+                truth_jagged_dict[k[len(truth_prefix):]] = v.array()
+            
+            for k, v in tqdm(reco_tree.items()):
+                if not k.startswith(reco_prefix):
+                    continue
+                reco_jagged_dict[k[len(reco_prefix):]] = v.array()
+            
+            truth_jagged_dict['truth_vtxID'] = truth_jagged_dict.pop('vtxID')
+
+            coords = ['d0', 'z0', 'phi', 'theta', 'qp']
+            scale = np.array([0.05, 500, 6, 2, 4])
+            for n in tqdm(range(len(truth_tree[0].array()))):
+                truth_df_dict = {k: truth_jagged_dict[k][n] for k in truth_jagged_dict.keys()}
+                reco_df_dict = {l: reco_jagged_dict[l][n] for l in reco_jagged_dict.keys()}
+                
+                truth_flat_event = pd.DataFrame(truth_df_dict)
+                reco_flat_event = pd.DataFrame(reco_df_dict)
+                truth_flat_event['truth_semantic_label'] = [1] * len(truth_flat_event)
+                
+                match_idx = get_match_idx(truth_flat_event, reco_flat_event)
+                truth_flat_event['reco_AMVF_vtxID'] = match_idx
+                truth_flat_event['reco_semantic_label'] = [1] * len(truth_flat_event)
+                
+                idx_not_found = truth_flat_event['reco_AMVF_vtxID'] == -1
+                truth_flat_event.loc[idx_not_found,'reco_semantic_label'] = [0]*len(truth_flat_event['reco_semantic_label'].loc[idx_not_found])
+                
+                truth_flat_event[coords] /= scale
+                truth_flat_event.to_pickle(raw_data_dir / f'event_{n+ni:05}.pkl')
             ni += n + 1
